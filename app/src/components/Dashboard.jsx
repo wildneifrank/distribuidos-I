20a73206870543c444a6180dc5b9b5db10bda23d
wildneifrank <wildneif2@gmail.com>
2023-11-08 16:27:54 -0300

    add lampada e temperatura

--- app/src/components/Dashboard.jsx
+++ app/src/components/Dashboard.jsx
@@ -6,6 +6,9 @@ import Switch from "@mui/material/Switch";
 
 const Dashboard = () => {
   const [light, setLight] = useState(false);
+  const [sensor, setSensor] = useState(false);
+  const [sound, setSound] = useState(false);
+
   const RedSwitch = styled(Switch)(({ theme }) => ({
     "& .MuiSwitch-switchBase.Mui-checked": {
       color: red[800],
@@ -39,13 +42,17 @@ const Dashboard = () => {
           />
         </div>
       </div>
-      <div className="rounded-md bg-white shadow-lg h-auto lg:h-1/6 p-6 border-slate-100 border dark:bg-slate-800 duration-500 ease-in-out">
+
+      <div className="rounded-md bg-white shadow-lg h-auto lg:h-1/6 p-6 border-slate-100 border dark:bg-slate-800 duration-500 ease-in-out flex flex-col gap-2">
         <div className="w-full text-red-800 text-lg font-medium h-auto dark:text-white duration-500 ease-in-out">
           Lâmpada
         </div>
-        <div className="w-full flex gap-3 justify-start items-center">
-          <div className="text-md font-medium text-red-800 dark:text-white duration-500 ease-in-out">
-            {light ? "Ligado" : "Desligado"}
+        <div className="w-full flex flex-col justify-center items-start">
+          <div className="flex gap-3">
+            <div className="text-slate-400 font-medium">Status:</div>
+            <div className="text-md font-medium text-red-800 dark:text-white duration-500 ease-in-out">
+              {light ? "Ligado" : "Desligado"}
+            </div>
           </div>
           <RedSwitch
             {...label}
@@ -57,40 +64,62 @@ const Dashboard = () => {
           />
         </div>
       </div>
-      <div className="rounded-md bg-white shadow-lg py-3 justify-between h-auto sm:py-3 sm:h-auto flex items-center px-6 border-slate-100 border flex-wrap dark:bg-slate-800 duration-500 ease-in-out">
-        <div className="w-full h-auto flex flex-col sm:w-2/3 lg:w-8/12">
-          <div className="text-md text-slate-400 font-medium">lorem</div>
-          <div className="text-xl text-red-800 font-semibold dark:text-white duration-500 ease-in-out">
-            Lorem Ipsum
+      <div className="rounded-md bg-white shadow-lg h-auto lg:h-1/6 p-6 border-slate-100 border dark:bg-slate-800 duration-500 ease-in-out flex flex-col gap-2">
+        <div className="w-full text-red-800 text-lg font-medium h-auto dark:text-white duration-500 ease-in-out">
+          Temperatura
+        </div>
+        <div className="w-full flex flex-col justify-center items-start">
+          <div className="flex gap-3">
+            <div className="text-slate-400 font-medium">Status:</div>
+            <div className="text-md font-medium text-red-800 dark:text-white duration-500 ease-in-out">
+              {sensor ? "Ligado" : "Desligado"}
+            </div>
           </div>
-          <input
-            type="file"
-            name="file"
-            id="file"
-            className="hidden"
-            onChange={() => {
-              toast.success("Arquivo upado com sucesso!");
+          <div className="flex gap-3">
+            <div className="text-slate-400 font-medium">Valor:</div>
+            <div className="text-md font-medium text-red-800 dark:text-white duration-500 ease-in-out">
+              20°C
+            </div>
+          </div>
+          <div className="flex gap-3">
+            <div className="text-slate-400 font-medium">Ar-condicionado:</div>
+            <div className="text-md font-medium text-red-800 dark:text-white duration-500 ease-in-out">
+              23°C
+            </div>
+            <div className="flex gap-1">
+              <i className="fa-solid fa-plus p-1 cursor-pointer rounded-full border text-red-800 border-red-800 bg-white hover:bg-red-800 hover:text-white duration-500 ease-in-out dark:bg-slate-800 dark:border-white dark:text-white dark:hover:bg-slate-700 "></i>
+              <i className="fa-solid fa-minus p-1 cursor-pointer rounded-full border text-red-800 border-red-800 bg-white hover:bg-red-800 hover:text-white duration-500 ease-in-out dark:bg-slate-800 dark:border-white dark:text-white dark:hover:bg-slate-700 "></i>
+            </div>
+          </div>
+          <RedSwitch
+            {...label}
+            checked={sensor}
+            className="duration-500 ease-in-out"
+            onClick={() => {
+              setSensor(!sensor);
             }}
           />
-          <label
-            htmlFor="file"
-            className="px-4 py-2 lg:w-2/12 sm:w-3/12 text-center rounded-lg shadow-lg border border-red-800 text-red-800 tetx-lg font-semibold duration-500 ease-in-out hover:bg-red-800 hover:text-white cursor-pointer dark:text-white dark:bg-red-800 dark:hover:text-red-800 dark:hover:bg-white"
-          >
-            Anexar Arquivo
-          </label>
         </div>
       </div>
-      <div className="rounded-md bg-white text-red-800 dark:text-white font-medium shadow-lg h-auto px-6 py-4 flex flex-col gap-2 border-slate-100 border dark:bg-slate-800 duration-500 ease-in-out">
-        <div className="w-full text-lg h-auto font-semibold">Lorem Ipsum</div>
-        <div className="w-full h-auto border border-red-800 rounded-md shadow-md">
-          <div className="w-full h-auto text-md flex gap-5 bg-red-800 text-white py-2 px-3">
-            <div className="w-1/4 sm:w-2/6 flex items-center">Categoria</div>
-            <div className="w-1/4 sm:w-2/6 flex items-center">
-              Velocidade Média
+      <div className="rounded-md bg-white shadow-lg h-auto lg:h-1/6 p-6 border-slate-100 border dark:bg-slate-800 duration-500 ease-in-out flex flex-col gap-2">
+        <div className="w-full text-red-800 text-lg font-medium h-auto dark:text-white duration-500 ease-in-out">
+          Lâmpada
+        </div>
+        <div className="w-full flex flex-col justify-center items-start">
+          <div className="flex gap-3">
+            <div className="text-slate-300 font-medium">Status:</div>
+            <div className="text-md font-medium text-red-800 dark:text-white duration-500 ease-in-out">
+              {light ? "Ligado" : "Desligado"}
             </div>
-            <div className="w-1/4 sm:w-2/6 flex items-center">Placa</div>
-            <div className="w-7 invisible">info</div>
           </div>
+          <RedSwitch
+            {...label}
+            checked={light}
+            className="duration-500 ease-in-out"
+            onClick={() => {
+              setLight(!light);
+            }}
+          />
         </div>
       </div>
     </div>
