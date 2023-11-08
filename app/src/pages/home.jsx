7292833154a16474cfd178801848968ca0da3dc2
wildneifrank <wildneif2@gmail.com>
2023-11-08 13:28:03 -0300

    add sidebar

---
+++ app/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+const home = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default home;
