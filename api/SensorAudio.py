95b4ebf911c4a1e925f4cd6b1e1d0d437f6ea35b
cmatheuslf <cmatheuslf@alu.ufc.br>
2023-11-11 16:28:38 -0300

    upando todos os  sensores

---
+++ api/SensorAudio.py
@@ -0,0 +1,28 @@
+import pika
+
+class SensorAudio:
+    def __init__(self) -> None:
+        pass
+
+    def verificaAudio(self):
+        Audio = input('Tem alguém aí? Digite 1 para sim, 0 para não: ')
+        return Audio
+
+# Configurar a conexão RabbitMQ
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+# Declarar uma fila chamada 'Audio_queue'
+channel.queue_declare(queue='Audio_queue')
+
+while True:
+    sensor = SensorAudio()
+    Audio = sensor.verificaAudio()
+    print('Presença:', Audio)
+
+    # Publicar a mensagem no RabbitMQ
+    channel.basic_publish(exchange='', routing_key='Audio_queue', body=Audio)
+    print(f'Mensagem {Audio} publicada na fila')
+
+# Fechar a conexão com o RabbitMQ
+connection.close()
--- api/SensorTemperatura.py
+++ api/SensorTemperatura.py
@@ -4,25 +4,25 @@ class SensorTemperatura:
     def __init__(self) -> None:
         pass
 
-    def verificaPresenca(self):
-        presenca = input('Tem alguém aí? Digite 1 para sim, 0 para não: ')
-        return presenca
+    def verificaTemperatura(self):
+        Temperatura = input('Tem alguém aí? Digite 1 para sim, 0 para não: ')
+        return Temperatura
 
 # Configurar a conexão RabbitMQ
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-# Declarar uma fila chamada 'presenca_queue'
+# Declarar uma fila chamada 'Temperatura_queue'
 channel.queue_declare(queue='temperatura_queue')
 
 while True:
     sensor = SensorTemperatura()
-    presenca = sensor.verificaPresenca()
-    print('Presença:', presenca)
+    Temperatura = sensor.verificaTemperatura()
+    print('Presença:', Temperatura)
 
     # Publicar a mensagem no RabbitMQ
-    channel.basic_publish(exchange='', routing_key='temperatura_queue', body=presenca)
-    print(f'Mensagem {presenca} publicada na fila')
+    channel.basic_publish(exchange='', routing_key='temperatura_queue', body=Temperatura)
+    print(f'Mensagem {Temperatura} publicada na fila')
 
 # Fechar a conexão com o RabbitMQ
 connection.close()
