fb5fd443825285f168b04719a0f1fa0335e8f01b
gabisponciano <114237253+gabisponciano@users.noreply.github.com>
2023-11-11 21:42:22 -0300

    Update homeassistant.py

--- api/homeassistant.py
+++ api/homeassistant.py
@@ -2,11 +2,16 @@ import socket
 import grpc
 from proto import messages_pb2
 from proto import messages_pb2_grpc
+from flask_cors import CORS #Route
+from flask import Flask, jsonify, request #Route
 import pika
 import threading
 import csv
 import json
 
+# Route
+app = Flask(__name__)
+CORS(app)
 # Nome do arquivo JSON
 nome_arquivo = 'objs.json'
 
@@ -107,4 +112,52 @@ def run():
     channel_ss.close()
 
 if __name__ == "__main__":
+    @app.route('/objetos', methods=['GET'])
+    def obter_status():
+        return jsonify(objetos)
+
+#Rota para pegar um "objeto" determinado
+    @app.route('/objetos/<string:tipo>', methods=['GET'])
+    def obter_objeto(tipo):
+        for objeto in objetos:
+            if objeto['tipo'] == tipo:
+                return jsonify(objeto)
+        return jsonify({'mensagem': 'Objeto não encontrado'}), 404
+
+#Rota para alterar um "objeto" determinado
+    @app.route('/objetos/<string:tipo>/editar', methods=['PUT'])
+    def editar_objeto(tipo):
+        dados_recebidos = request.get_json()
+        for objeto in objetos:
+            if objeto['tipo'] == tipo:
+                objeto.update(dados_recebidos)
+                return jsonify(objeto)
+        return jsonify({'mensagem': 'Objeto não encontrado'}), 404
+
+#Rota para editar o ar condicionado
+    @app.route('/objetos/temperatura/ar_condicionado', methods=['PUT'])
+    def editar_ar_condicionado():
+        dados_recebidos = request.get_json()
+        ar_condicionado = objetos[0]['Ar condicionado']
+        ar_condicionado.update(dados_recebidos)
+        return jsonify(ar_condicionado)
+
+#Rota para editar a caixa de som
+    @app.route('/objetos/som/caixa_de_som', methods=['PUT'])
+    def editar_caixa_de_som():
+        dados_recebidos = request.get_json()
+        caixa_de_som = objetos[1]['Caixa de som']
+        caixa_de_som.update(dados_recebidos)
+        return jsonify(caixa_de_som)
+
+#Rota para editar a Lâmpada
+    @app.route('/objetos/luz/lampada', methods=['PUT'])
+    def editar_lampada():
+        dados_recebidos = request.get_json()
+        lampada = objetos[2]['lampada']
+        lampada.update(dados_recebidos)
+        return jsonify(lampada)
+    
+    app.run(port=3002, host='localhost', debug=True)
+    
     run()
