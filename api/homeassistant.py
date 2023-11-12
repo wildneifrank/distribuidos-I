00c03b9beae042aef279d6c3eb144bc71c5a44df
cmatheuslf <cmatheuslf@alu.ufc.br>
2023-11-12 00:28:13 -0300

    objs consertados nas rotas

--- api/homeassistant.py
+++ api/homeassistant.py
@@ -114,53 +114,62 @@ def run():
     channel_lp.close()
     channel_ss.close()
 
-if __name__ == "__main__":
-    @app.route('/objetos', methods=['GET'])
-    def obter_status():
-        return jsonify(objetos)
 
+""" @app.route('/objetos', methods=['GET'])
+def obter_status():
+    return jsonify(objetos)
+"""
 #Rota para pegar um "objeto" determinado
-    @app.route('/objetos/<string:tipo>', methods=['GET'])
-    def obter_objeto(tipo):
-        for objeto in objetos:
-            if objeto['tipo'] == tipo:
-                return jsonify(objeto)
-        return jsonify({'mensagem': 'Objeto não encontrado'}), 404
+@app.route('/objetos/<string:tipo>', methods=['GET'])
+def obter_objeto(tipo):
+    if(tipo == 'lampada'):
+        objeto = json.load('lampada.json')
+        return jsonify(objeto)
+    elif(tipo == 'caixaSom'):
+        objeto = json.load('caixaSom.json')
+        return jsonify(objeto)
+    elif(tipo == 'arcondicionado'):
+        objeto = json.load('arcondicionado.json')
+        return jsonify(objeto)
+    return jsonify({'mensagem': 'Objeto não encontrado'}), 404
 
-#Rota para alterar um "objeto" determinado
-    @app.route('/objetos/<string:tipo>/editar', methods=['PUT'])
-    def editar_objeto(tipo):
-        dados_recebidos = request.get_json()
-        for objeto in objetos:
-            if objeto['tipo'] == tipo:
-                objeto.update(dados_recebidos)
-                return jsonify(objeto)
-        return jsonify({'mensagem': 'Objeto não encontrado'}), 404
+""" #Rota para alterar um "objeto" determinado
+@app.route('/objetos/<string:tipo>/editar', methods=['PUT'])
+def editar_objeto(tipo):
+    dados_recebidos = request.get_json()
+    for objeto in objetos:
+        if objeto['tipo'] == tipo:
+            objeto.update(dados_recebidos)
+            return jsonify(objeto)
+    return jsonify({'mensagem': 'Objeto não encontrado'}), 404 """
 
 #Rota para editar o ar condicionado
-    @app.route('/objetos/temperatura/ar_condicionado', methods=['PUT'])
-    def editar_ar_condicionado():
-        dados_recebidos = request.get_json()
-        ar_condicionado = objetos[0]['Ar condicionado']
-        ar_condicionado.update(dados_recebidos)
-        return jsonify(ar_condicionado)
+@app.route('/objetos/temperatura/ar_condicionado', methods=['PUT'])
+def editar_ar_condicionado():
+    objeto = json.load('json/arcondicionado.json')
+    dados_recebidos = request.get_json()
+    ar_condicionado = objeto['Ar condicionado']
+    ar_condicionado.update(dados_recebidos)
+    return jsonify(ar_condicionado)
 
 #Rota para editar a caixa de som
-    @app.route('/objetos/som/caixa_de_som', methods=['PUT'])
-    def editar_caixa_de_som():
-        dados_recebidos = request.get_json()
-        caixa_de_som = objetos[1]['Caixa de som']
-        caixa_de_som.update(dados_recebidos)
-        return jsonify(caixa_de_som)
+@app.route('/objetos/som/caixa_de_som', methods=['PUT'])
+def editar_caixa_de_som():
+    objeto = json.load('json/caixaSom.json')
+    dados_recebidos = request.get_json()
+    caixa_de_som = objeto['Caixa de som']
+    caixa_de_som.update(dados_recebidos)
+    return jsonify(caixa_de_som)
 
 #Rota para editar a Lâmpada
-    @app.route('/objetos/luz/lampada', methods=['PUT'])
-    def editar_lampada():
-        dados_recebidos = request.get_json()
-        lampada = objetos[2]['lampada']
-        lampada.update(dados_recebidos)
-        return jsonify(lampada)
-    
-    app.run(port=3002, host='localhost', debug=True)
-    
-    run()
+@app.route('/objetos/luz/lampada', methods=['PUT'])
+def editar_lampada():
+    objeto = json.load('json/lampada.json')
+    dados_recebidos = request.get_json()
+    lampada = objeto['lampada']
+    lampada.update(dados_recebidos)
+    return jsonify(lampada)
+
+app.run(port=3002, host='localhost', debug=True)
+
+run()
